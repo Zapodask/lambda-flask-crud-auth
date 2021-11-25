c80eb96468b5b1f538ba5a9d6b731b99b352081f
Zapodask <joaopedroemerim@gmail.com>
2021-11-25 00:30:56 -0300

    Dia 04 - config crud

--- src/app.py
+++ src/app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restx import Api
 
-from routes import users
+from .routes import users as nsUsers
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 # Namespaces
-api.add_namespace(users)
+api.add_namespace(nsUsers)
--- src/routes.py
+++ src/routes.py
@@ -14,31 +14,30 @@ project_name = os.getenv('PROJECT_NAME')
 dynamodb = resource('dynamodb')
 table = dynamodb.Table(f'{project_name}-users')
 
-users = Namespace('users/', description='Users')
+def now():
+    return str(datetime.now(timezone('America/Sao_Paulo')))
 
-userReturn = users.model('User', {
+users = Namespace('users', description='Users routes')
+
+userModel = users.model('User', {
     'username': fields.String(),
     'password': fields.String(),
-    'created_at': fields.String()
+    'created_at': fields.String(readonly=True),
+    'updated_at': fields.String(readonly=True)
 })
 
-userExpect = users.model('User', {
-    'username': fields.String(required=True),
-    'password': fields.String(required=True)
-})
 
 @users.route('/')
 class Index(Resource):
     @users.doc('list_users')
-    @users.marshal_list_with(userReturn)
+    @users.marshal_list_with(userModel)
     def get(self):
         response = table.scan()
 
-        return response['Items']
-
+        return response['Items'], 200
 
     @users.doc('store_user')
-    @users.expect(userExpect)
+    @users.expect(userModel)
     def post(self):
         data = request.get_json()
 
@@ -48,24 +47,22 @@ class Index(Resource):
             }
         )
 
-        print(len(response))
-        if len(response['Item']) > 0:
+        if 'Item' in response:
             return 'Username already exists', 400
 
-        data['created_at'] = str(datetime.now(timezone('America/Sao_Paulo')))
+        data['created_at'] = data['updated_at'] = now()
 
         table.put_item(Item=data)
 
         return 'User created', 200
 
 
-
-@users.route('/<str:username>')
+@users.route('/<string:username>')
 @users.param('username', 'Usename')
 @users.response(404, 'User not found')
 class Id(Resource):
     @users.doc('show_user')
-    @users.marshal_with(userReturn)
+    @users.marshal_with(userModel)
     def get(self, username):
         response = table.get_item(
             Key={
@@ -73,23 +70,31 @@ class Id(Resource):
             }
         )
 
-        return response['Item']
-
+        if 'Item' in response:
+            return response['Item'], 200
+        else:
+            users.abort(404, 'User not found')
 
     @users.doc('update_user')
-    @users.expect(userExpect)
-    @users.marshal_with(userReturn)
+    @users.marshal_with(userModel)
     def put(self, username):
+        username = username.replace('%20', ' ')
+
         data = request.get_json()
+
+        data['updated_at'] = now()
+
+        # Add verify name
 
         table.update_item(
             Key={
                 'username': username
             },
-            UpdateExpression='set username=:u, password=:p',
+            UpdateExpression='set username=:u, password=:p, updated_at:ua',
             ExpressionAttributeValues={
                 ':u': data['username'],
-                ':p': data['password']
+                ':p': data['password'],
+                ':ua': data['updated_at']
             }
         )
 
@@ -99,8 +104,7 @@ class Id(Resource):
             }
         )
 
-        return response['Item']
-
+        return response['Item'], 200
 
     @users.doc('delete_user')
     def delete(self, username):
