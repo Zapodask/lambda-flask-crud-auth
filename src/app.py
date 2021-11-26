ed644225ff70caa6a4c9321701a30ee8f5cfab3c
Zapodask <joaopedroemerim@gmail.com>
2021-11-26 01:48:01 -0300

    Dia 05 - update template, add login

--- src/app.py
+++ src/app.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restx import Api
 
+from flask_jwt_extended import JWTManager
+
+from datetime import timedelta
+
 from .routes import users as nsUsers
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+jwt = JWTManager(app)
 
 # Namespaces
 api.add_namespace(nsUsers)
--- src/routes.py
+++ src/routes.py
@@ -4,8 +4,14 @@ from boto3 import resource
 from datetime import datetime
 from pytz import timezone
 
-from dotenv import load_dotenv
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
@@ -41,6 +47,8 @@ class Index(Resource):
     def post(self):
         data = request.get_json()
 
+        data['password'] = generate_password_hash(data['password'])
+
         response = table.get_item(
             Key={
                 'username': data['username']
@@ -64,6 +72,8 @@ class Id(Resource):
     @users.doc('show_user')
     @users.marshal_with(userModel)
     def get(self, username):
+        username = username.replace('%20', ' ')
+
         response = table.get_item(
             Key={
                 'username': username
@@ -75,39 +85,10 @@ class Id(Resource):
         else:
             users.abort(404, 'User not found')
 
-    @users.doc('update_user')
-    @users.marshal_with(userModel)
-    def put(self, username):
-        username = username.replace('%20', ' ')
-
-        data = request.get_json()
-
-        data['updated_at'] = now()
-
-        # Add verify name
-
-        table.update_item(
-            Key={
-                'username': username
-            },
-            UpdateExpression='set username=:u, password=:p, updated_at:ua',
-            ExpressionAttributeValues={
-                ':u': data['username'],
-                ':p': data['password'],
-                ':ua': data['updated_at']
-            }
-        )
-
-        response = table.get_item(
-            Key={
-                'username': username
-            }
-        )
-
-        return response['Item'], 200
-
     @users.doc('delete_user')
     def delete(self, username):
+        username = username.replace('%20', ' ')
+
         table.delete_item(
             Key={
                 'username': username
@@ -115,3 +96,70 @@ class Id(Resource):
         )
 
         return 'User deleted', 204
+
+
+@users.route('/login')
+class Login(Resource):
+    @users.doc('login')
+    def post(self):
+        data = request.get_json()
+
+        response = table.get_item(
+            Key={
+                'username': data['username']
+            }
+        )
+
+        if data['password'] == response['password']:
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
+            return {'access_token': access_token, 'refresh_token': refresh_token}
+        else:
+            return 'Password incorrect', 400
+
+
+@users.route('/refresh-token')
+@jwt_required(refresh=True)
+class RefreshToken(Resource):
+    @users.doc('refresh_token')
+    def post(self):
+        identity = get_jwt_identity()
+        access_token = create_access_token(identity=identity)
+        refresh_token = create_refresh_token(identity=identity)
+        return {'access_token': access_token, 'refresh_token': refresh_token}
+
+
+@users.route('/change-password')
+@jwt_required()
+class ChangePassword(Resource):
+    @users.doc('update_user')
+    @users.marshal_with(userModel)
+    def put(self):
+        identity = get_jwt_identity()
+        data = request.get_json()
+
+        response = table.get_item(
+            Key={
+                'username': identity
+            }
+        )
+
+        check = check_password_hash(response['password'], data['password'])
+
+        if check != True:
+            return 'Wrong password', 400
+
+        data['updated_at'] = now()
+
+        table.update_item(
+            Key={
+                'username': identity
+            },
+            UpdateExpression='set password=:p, updated_at=:a',
+            ExpressionAttributeValues={
+                ':p': data['new_password'],
+                ':a': data['updated_at']
+            }
+        )
+
+        return 'Password changed', 200
